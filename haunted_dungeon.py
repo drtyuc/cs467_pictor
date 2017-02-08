06089cb4479eb0c79ce535245a22a89d03adcb2c
Andrew Bagwell <andrewbagwell@Andrews-MacBook-Pro.local>
2017-02-08 08:55:37 -0600

    added generate room text to HD class

--- haunted_dungeon.py
+++ haunted_dungeon.py
@@ -68,6 +68,66 @@ class HauntedDungeon():
     def runit(self):
 	self.showMenu()
 
+    '''
+    Description: This function will generate the first portion for the display text for the game:
+    the room description, the objects available, the exits, and any ghosts that are already visible
+
+    '''
+    def generateText(self):
+
+        self.dm = DataManager() #this won't be the same instance that is running in the game module, so this probably won't work
+
+        #print header info
+
+        print "Haunted Dungeon\t\t\t", "Health: " , dm.getPlayerHealth()
+
+        if self.dm.isRoomDiscovered() == False:
+            #print appropriate room description
+
+            print self.dm.getRoomLongDescription()
+
+            #get the objects in the room and print the visible ones 
+
+            objectsAvailable = self.dm.getRoomObjects()
+
+            print "You see these items..."
+
+            for item in objectsAvailable:
+                if self.dm.isObjectVisible(item):
+                    print "*", item 
+
+            #get the room exits and print the visible 
+            print "You see these exits..."
+            roomExits = self.dm.getRoomsExits()
+
+            for exit in roomExits:
+                if self.dm.isExitVisible(exit):
+                    print "To the " + self.dm.getExitDirection(exit), "..." + self.dm.getExitLongDescription(exit)
+
+        else:
+            print dm.getRoomShortDescription()
+            objectsAvailable = self.dm.getRoomObjects()
+
+            print "You see these items..."
+
+            for item in objectsAvailable:
+                if self.dm.isObjectVisible(item):
+                    print "*", item 
+
+            print "You see these exits..."
+            roomExits = self.dm.getRoomsExits()
+
+            for exit in roomExits:
+                if self.dm.isExitVisible(exit):
+                    print "To the " + self.dm.getExitDirection(exit), "..." + self.dm.getExitShortDescription(exit)
+
+        ghosts = self.dm.getGhostsNames()
+
+        for g in ghosts:
+            if self.dm.getGhostLocation(g) == dm.getPlayerLocation():
+                if self.dm.isGhostVisible(g):
+                    print "You see " + dm.getGhostShortDescription(g) 
+
 
 
 """ MAIN """
